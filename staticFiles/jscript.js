40161f2239c14b507cce15ef31504986052bb814
Paratra <ming.song.cn@outlook.com>
2023-09-20 15:39:44 -0400

    update

--- staticFiles/jscript.js
+++ staticFiles/jscript.js
@@ -2,18 +2,18 @@ let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn")
 let nextBtn = document.getElementById("nextBtn")
 
-if (scrollContainer) {
-scrollContainer.addEventListener("wheel",(evt)=> {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
-}
+// if (scrollContainer) {
+// scrollContainer.addEventListener("wheel",(evt)=> {
+//     evt.preventDefault();
+//     scrollContainer.scrollLeft += evt.deltaY;
+// });
+// }
 
 backBtn.addEventListener("click",()=>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 310;
+    scrollContainer.scrollLeft -= 980;
 });
 nextBtn.addEventListener("click",()=>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 310;
+    scrollContainer.scrollLeft += 980;
 });
