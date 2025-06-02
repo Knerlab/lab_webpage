6c003db7ec71124c15543bae457c30e281b1850b
Paratra <ming.song.cn@outlook.com>
2025-06-02 15:38:32 -0400

    fix

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def teaching_lectures():
     return render_template('teaching_lectures.html') 
 
 if __name__=='__main__':
-    app.run(debug = True, port=8001)
+    app.run(debug = True, port=8000)
